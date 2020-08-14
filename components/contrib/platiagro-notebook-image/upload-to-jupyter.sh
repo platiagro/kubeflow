345855f0010c4f933f9ac72df09cbe919567662c
Fabio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2020-08-14 19:24:08 -0300

    Fix "Argument list too long" on upload-jupyter.sh

--- components/contrib/platiagro-notebook-image/upload-to-jupyter.sh
+++ components/contrib/platiagro-notebook-image/upload-to-jupyter.sh
@@ -6,4 +6,7 @@ curl -v -X PUT http://server.anonymous:80/notebook/anonymous/server/api/contents
 curl -v -X PUT http://server.anonymous:80/notebook/anonymous/server/api/contents/experiments/$1 -H 'Content-Type: application/json' -H 'X-XSRFToken: token' -H 'Cookie: _xsrf=token' --data '{"type":"directory"}'
 curl -v -X PUT http://server.anonymous:80/notebook/anonymous/server/api/contents/experiments/$1/operators -H 'Content-Type: application/json' -H 'X-XSRFToken: token' -H 'Cookie: _xsrf=token' --data '{"type":"directory"}'
 curl -v -X PUT http://server.anonymous:80/notebook/anonymous/server/api/contents/experiments/$1/operators/$2 -H 'Content-Type: application/json' -H 'X-XSRFToken: token' -H 'Cookie: _xsrf=token' --data '{"type":"directory"}'
-curl -v -X PUT http://server.anonymous:80/notebook/anonymous/server/api/contents/experiments/$1/operators/$2/$3 -H 'Content-Type: application/json' -H 'X-XSRFToken: token' -H 'Cookie: _xsrf=token' --data "{\"type\":\"notebook\", \"content\":$content}"
+echo "{\"type\":\"notebook\", \"content\":" > data.json
+cat output.ipynb >> data.json
+echo "}" >> data.json
+curl -v -X PUT http://server.anonymous:80/notebook/anonymous/server/api/contents/experiments/$1/operators/$2/$3 -H 'Content-Type: application/json' -H 'X-XSRFToken: token' -H 'Cookie: _xsrf=token' --data @data.json
