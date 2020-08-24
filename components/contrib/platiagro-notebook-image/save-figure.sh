8cc4cc809592e9c273a548fbd6f71541160ac130
Ricardo Shiguemi Hiramatsu <ricardoh@aquarius.cpqd.com.br>
2020-08-24 12:03:31 -0300

    Save figures automatically using notebook content

---
+++ components/contrib/platiagro-notebook-image/save-figure.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cat << EOF > save_figure.py
+#!/opt/conda/bin/python
+from json import loads
+from platiagro import save_figure
+with open("output.ipynb", "rb") as notebook_file:
+	notebook = loads(notebook_file.read())
+	cells = notebook['cells']
+	for cell in cells:
+		if 'outputs' in cell:
+			outputs = cell['outputs']
+			for output in outputs:
+				if 'data' in output:
+					data = output['data']
+					keys = data.keys()
+					for key in keys:
+						if 'image' in key:
+							save_figure(figure=data[key], extension=key.split('/')[1])
+EOF
+
+chmod 755 save_figure.py
+./save_figure.py
