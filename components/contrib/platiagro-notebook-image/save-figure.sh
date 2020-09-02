926e75676e52290fb4c5f291c17419d97e11ea53
Ricardo Shiguemi Hiramatsu <ricardoh@aquarius.cpqd.com.br>
2020-09-02 09:42:54 -0300

    Save html figures

--- components/contrib/platiagro-notebook-image/save-figure.sh
+++ components/contrib/platiagro-notebook-image/save-figure.sh
@@ -16,7 +16,13 @@ with open("output.ipynb", "rb") as notebook_file:
 					data = output['data']
 					keys = data.keys()
 					for key in keys:
-						if 'image' in key:
+						if 'html' in key:
+							html = data[key]
+							if 'script' not in html[0]:
+								plotly_figure = ''.join(html).replace('["plotly"]', '["https://cdn.plot.ly/plotly-latest.min.js"]')
+								html_figure = '<html><head><script src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.js"></script></head><body>' + plotly_figure + '</body></html>'
+								save_figure(figure=html_figure, extension='html')
+						elif 'image' in key:
 							save_figure(figure=data[key], extension=key.split('/')[1])
 
 EOF
