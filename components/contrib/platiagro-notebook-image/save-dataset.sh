6517acffe447126f60e2514d3cff16e3b3b86db9
Fabio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2020-10-29 11:28:38 -0300

    Copies changes from feature/platiagro-notebook-image-v0.0.2
    Contents:
    - Dockerfile for Jupyter notebook image (targeting SDK v0.0.2)
    - Github Actions (build/push docker image)

---
+++ components/contrib/platiagro-notebook-image/save-dataset.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cat << EOF > save_dataset.py
+#!/opt/conda/bin/python
+from glob import glob
+from platiagro import save_dataset
+import pandas as pd
+
+
+csv_path = glob("/tmp/data/*.csv")[0]
+dataset = csv_path.split('/')[-1]
+
+try:
+    df = pd.read_csv(csv_path)
+    save_dataset(name=dataset, df=df)
+except pd.errors.EmptyDataError:
+    content = open(csv_path, "rb")
+    save_dataset(name=dataset, data=content)
+
+EOF
