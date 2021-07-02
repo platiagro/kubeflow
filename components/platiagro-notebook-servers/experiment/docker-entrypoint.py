373fc99de654ff88e142bf60117da14d803fb3de
Fabio Beranizo <fabiol@cpqd.com.br>
2021-07-02 19:06:48 -0300

    Do not download_dataset and save_dataset when PARAMETER_dataset is 'Falseable'
    Also don't execute_notebook when NOTEBOOK_PATH is 'Falseable'.
    != "" caused issues since dataset and notebook_path could be None.

--- components/platiagro-notebook-servers/experiment/docker-entrypoint.py
+++ components/platiagro-notebook-servers/experiment/docker-entrypoint.py
@@ -246,10 +246,10 @@ def main():
 
     exception = None
 
-    if dataset != "":
+    if dataset:
         download_dataset(dataset)
 
-    if notebook_path != "":
+    if notebook_path:
         try:
             execute_notebook(notebook_path, output_path)
         except papermill.exceptions.PapermillExecutionError as e:
@@ -262,7 +262,7 @@ def main():
 
         save_figures(output_path)
 
-    if dataset != "":
+    if dataset:
         save_dataset(dataset)
 
     if exception is not None:
